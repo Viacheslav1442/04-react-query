58639a4b96a2577c2138a990eac28e3b8bb8c0ed
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-07-05 10:55:49 +0300

    tsx

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -7,3 +7,16 @@ export interface Movie {
     release_date: string;
     vote_average: number;
 }
+
+export interface Movie {
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
+}
+
+export interface MoviesResponse {
+    page: number;
+    results: Movie[];
+    total_pages: number;
+}
