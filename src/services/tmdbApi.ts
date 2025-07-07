cb9062653f706a875cbc985a0e65e403bbba4bef
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-07-07 20:01:28 +0300

    tsx

--- src/services/tmdbApi.ts
+++ src/services/tmdbApi.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import type { MovieResponse } from '../types/movie';
 
-const API_KEY = import.meta.env.VITE_API_TOKEN;
+const BEARER_TOKEN = import.meta.env.VITE_API_TOKEN;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const fetchMovies = async (query: string, page = 1): Promise<MovieResponse> => {
     const response = await axios.get(`${BASE_URL}/search/movie`, {
+        headers: {
+            Authorization: `Bearer ${BEARER_TOKEN}`,
+        },
         params: {
-            api_key: API_KEY,
             query,
             page,
         },
