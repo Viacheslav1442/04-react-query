e2caa896dd4b5e135f95aab21c79711271b84bee
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-07-07 21:04:24 +0300

    tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import { fetchMovies } from '../../services/tmdbApi';
-import type { MovieResponse } from '../../types/movie';
+import type { Movie, MovieResponse } from '../../types/movie';
 import MovieGrid from '../MovieGrid/MovieGrid';
 import SearchBar from '../SearchBar/SearchBar';
 import ReactPaginate from 'react-paginate';
+import MovieModal from '../MovieModal/MovieModal';
 import css from './App.module.css';
 
 export default function App() {
     const [query, setQuery] = useState('');
     const [searchTerm, setSearchTerm] = useState(query);
     const [page, setPage] = useState(1);
+    const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null); // ✅
 
     const { data, isLoading, isError } = useQuery<MovieResponse>({
         queryKey: ['movies', searchTerm, page],
         queryFn: () => fetchMovies(searchTerm, page),
-
     });
 
     const handlePageClick = ({ selected }: { selected: number }) => {
@@ -40,7 +41,10 @@ export default function App() {
 
             {data && (
                 <>
-                    <MovieGrid movies={data.results} onSelect={() => { }} />
+                    <MovieGrid
+                        movies={data.results}
+                        onSelect={setSelectedMovie} // ✅
+                    />
                     {data.total_pages > 1 && (
                         <ReactPaginate
                             pageCount={data.total_pages}
@@ -55,6 +59,13 @@ export default function App() {
                         />
                     )}
                 </>
+            )}
+
+            {selectedMovie && (
+                <MovieModal
+                    movie={selectedMovie}
+                    onClose={() => setSelectedMovie(null)}
+                />
             )}
         </div>
     );
